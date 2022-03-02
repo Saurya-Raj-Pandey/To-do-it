c6f72b5eb02d44073ff2fc17406a3b925c85157e
Saurya Raj Pandey <sauryap77@gmail.com>
2022-03-02 12:43:40 +0545

    Updates

---
+++ precache-manifest.928604ac3e0468a8e8d226de0ee9525b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfca331395686b65e6db37ac7ffd12f9",
+    "url": "/To-do-it/index.html"
+  },
+  {
+    "revision": "00a5ae0d029aa4b78e9f",
+    "url": "/To-do-it/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b07234422a53f771f140",
+    "url": "/To-do-it/static/js/2.4f766041.chunk.js"
+  },
+  {
+    "revision": "77ff2abf6da91ad83241",
+    "url": "/To-do-it/static/js/3.153d6254.chunk.js"
+  },
+  {
+    "revision": "00a5ae0d029aa4b78e9f",
+    "url": "/To-do-it/static/js/main.2ddd9319.chunk.js"
+  },
+  {
+    "revision": "b0ac91e4e2a224b7440f",
+    "url": "/To-do-it/static/js/runtime~main.baf23497.js"
+  }
+]);
